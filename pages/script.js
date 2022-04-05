47f57a3e6b5d09f2c4ffc019c6cc098eb0579228
Ruslan Afinshakov <shador@mail.ru>
2022-04-05 22:57:50 +1000

    feat: добавлена разметка для формы и сверстаны кнопки удаления картинок

--- pages/script.js
+++ pages/script.js
@@ -2,24 +2,55 @@ const profileEditBtn = document.querySelector('.profile__edit-btn');
 const profileEditPopup = document.querySelector('.popup');
 const profileCloseBtn = document.querySelector('.popup__close-btn');
 const formElement = document.querySelector('.popup__form');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__name-description');
 let nameInput = formElement.querySelector('#user-name');
 let jobInput = formElement.querySelector('#user-description');
 
+// Открытие попапа
 function openProfilePopup() {
     profileEditPopup.classList.add('popup_opened')
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 };
 profileEditBtn.addEventListener('click', openProfilePopup);
+//---------------------------------------------
 
+//Закрытие попапа
 function closeProfilePopup() {
     profileEditPopup.classList.remove('popup_opened')
 };
 profileCloseBtn.addEventListener('click', closeProfilePopup);
+//------------------------------------------------
 
+//Сохранение информации о себе
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -33,6 +64,8 @@ function formSubmitHandler (evt) {
   closeProfilePopup()
 };
 formElement.addEventListener('submit', formSubmitHandler);
+//-------------------------------------------------------------
+
 
 
 let likes = document.querySelectorAll('li.gallery-cards__card div.gallery-cards__description gallery-cards__like-btn');
